9b7dd265b492c9a9a366872b0f8cc8c7dbf47c37
jjoshi8 <jjoshi8@illinois.edu>
2022-03-24 23:55:34 -0500

    Added error calculation for Yoshida method

--- error_calculations.py
+++ error_calculations.py
@@ -1,1 +1,38 @@
+import numpy as np
+
+from numerical_methods import ivp_yoshida
+
+def ivp_yoshida_error(u_0, T, delta_t, delta_t_baseline):
+
+    """
+    Implements the error in the predicted final system state for the 4th-order Yoshida integrator.
+  
+    Parameters
+    ----------
+    u_0 : array
+        1 x N array defining the initial state vector u_0
+    T : float_like
+        Final time T
+    delta_t : float_like
+        Time step size where delta_t = t_{k+1} - t_k
+        
+    Returns
+    -------
+    err : float
+        Error calculated as ||u_final_predicted - u_final_actual|| / ||u_final_actual||
+        
+    """
+
+    # get full K x N arrays
+    u_delta_t, times = ivp_yoshida(u_0, T, delta_t)
+    u_delta_t_baseline, times_baseline = ivp_yoshida(u_0, T, delta_t_baseline)
+
+    # extract only the final 1 x N array
+    u_final_delta_t = u_delta_t[-1]
+    u_final_delta_t_baseline = u_delta_t_baseline[-1]
+
+    # compute the error
+    err = la.norm(u_final_delta_t - u_final_delta_t_baseline) / la.norm(u_final_delta_t_baseline)
+
+    return err
 
--- output.py
+++ output.py
@@ -8,7 +8,7 @@ from numerical_methods import runge_kutta
 rm = 3396.2
 
 # define initial states for bodies [km] and [km/s]
-u_0_sat = np.array([44500, 0, 0, 0, 1.0735*0.25, 0])
+u_0_sat = np.array([5000, 0, 0, 0, 3, 0])
 u_0_mars = np.array([0, 0, 0, 0, 0, 0])
 u_0_pho = np.array([-1.05*rm, -0.676*rm, -2.47*rm, 1, 1, 1])
 u_0_dei = np.array([5.89*rm, 3.31*rm, 1.46*rm, 1, 1, 1])
@@ -16,16 +16,24 @@ u_0_sun = np.array([10000*rm, 10000*rm, 10000*rm, 1, 1, 1])
 u_0 = np.array([u_0_sat, u_0_mars, u_0_pho, u_0_dei, u_0_sun])
 
 # define time step [s]
-delta_t = 1
+delta_t = 15
 
 # define time [s]
-T = 111 * 60 * 50
+T = 111 * 60 * 2
 
 # retrieve states using the Yoshida method
 u_predicted, times = ivp_yoshida(u_0, T, delta_t)
 print(u_predicted[320, 0, 3:5]) # -1.41067131e+06
 
-plt.plot(u_predicted[:, 0, 0], u_predicted[:, 0, 1])
+ax = plt.axes(projection='3d')
+u, v = np.mgrid[0:2*np.pi:2000j, 0:np.pi:2000j]
+x = rm * np.cos(u) * np.sin(v)
+y = rm * np.sin(u) * np.sin(v)
+z = rm * np.cos(v)
+ax.plot_wireframe(x, y, z, color='brown')
+ax.plot(u_predicted[:, 0, 0], u_predicted[:, 0, 1], u_predicted[:, 0, 2])
+
+# plt.plot(u_predicted[:, 0, 0], u_predicted[:, 0, 1])
 # plt.plot(times, u_predicted[:, 0, 1])
 plt.show()
 # plt.show(block=False)
